93a38467a65489fe67eca1a22f4c420704cfe89b
JD Tadlock <jt7903@gmail.com>
2024-06-18 13:30:48 -0400

    finished basic operators (logical, comparisons)

--- js/script.js
+++ js/script.js
@@ -1,65 +1,28 @@
-// Javascript works with or has a set of different values 
-// A word or multiple words wrapped in quotes is called a String value type
-// A number is called a Number value type
-
-// Primitive Value Types
 var firstName = 'JD'; // String
 var lastName = 'Tadlock';
 var age = 44; // Number
-var isADev = true; // Boolean
-var un = undefined; // Undefined is a value type that JS will give you when there is no declared or assigned value (Provided By JS)
-var n = null; // Programmer's representation of nothing
-var notANumber = NaN; // Not A Number - This is provided when you try to process a mathmatical equation and the result is not a valid number (Provided By JS)
+var names = ['John', 'Ann', 'Bob', 'Frank', 'Jane', 'Jimmy', 'Billy', 'Julie', 'Will'];
 
-var nothing;
-var another;
-var oneMore;
+// Comparison Operators - Return a boolean based on the result of your comparison
+// console.log(age > 30);
+// console.log(age < 30);
+// console.log(age == 44);
+// console.log(age == '44'); // Double equals checks only value
+// console.log(age === 44); // Triple equals checks both value and type
 
-// console.log(age); // Using a variable within your code is called a reference
+// console.log(age >= 40);
+// console.log(age <= 40);
 
-// age = age + 1;
-// age += 5; // Increase a variable by any numerical amount
-age++; // Increase a variable by one
+// console.log(age != 43);
+// console.log(age !== 43);
 
-// console.log(age);
+// Falsey Values
+// 0, null, undefined, '', NaN, false
 
-age--; // Decrease number by one
+// Logical Operators - Return a value on either the left side or the right side of the operator
+// Which value is equated is based truth (truthy vs falsey)
+console.log(0 || null || '' || -5 || undefined); // If anyone of these is true, then perform some action
 
-// console.log(age);
+console.log(1 && age && 100 && null);
 
-// age *= 5;
-
-var fullName = firstName + ' ' + lastName;
-
-var names = ['John', 'Ann', 'Bob', 'Frank', 'Jane', 'Jimmy', 'Billy', 'Julie', 'Will']; // Array
-var ages = [30, 24, 55, 99];
-
-
-// names.push('Susan');
-
-// console.log(names);
-
-// names.unshift('Eli');
-
-console.log(names);
-
-// names.pop();
-// names.shift();
-names.splice(5, 1);
-
-console.log(names);
-
-
-
-// console.log(names[names.length - 1]);
-
-// console.log(names);
-
-// names[1] = 'Joe';
-
-// console.log(names);
-
-// ages[2] += 10;
-
-// console.log(ages)
-
+console.log(!'');
