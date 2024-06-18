daef4c7bc9ecd13b103decd254916954be15ffc4
JD Tadlock <jt7903@gmail.com>
2024-06-18 10:23:04 -0400

    finished console basics

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Javascript works with or has a set of different values 
+// A word or multiple words wrapped in quotes is called a String value type
+// A number is called a Number value type
+var firstName = 'JD';
+
+console.log(typeof 3);
+
+console.log(5 - 5);
+
+console.log(firstName);
