49de31156d2a3565b7d204f10f0ae6354575db62
JD Tadlock <jt7903@gmail.com>
2024-06-20 14:33:24 -0400

    started functions overview

--- js/script.js
+++ js/script.js
@@ -1,1 +1,41 @@
 
+
+// function myFirstFunc() {
+//   console.log('function has been called');
+// }
+
+// myFirstFunc();
+
+// function add(num1, num2) {
+//   console.log(num1 + num2);
+// }
+
+// add(5, 10);
+
+// add(30, 27);
+
+// function printValues(num, name, boolean, another) {
+//   console.log(another);
+// }
+
+// printValues(10, 'Allen', false, 'another');
+
+// const someStr = 'some string';
+
+// console.log(someStr[6]);
+
+function printNames(names) {
+  for (const name of names) {
+    console.log(name);
+  }
+}
+
+// printNames(['Bob', 'Jimmy', 'Julie']);
+
+// printNames(['Jake', 'Frank', 'Katie']);
+
+const someName = 'JD';
+const h1 = document.querySelector('h1');
+
+h1.innerText = someName;
+
