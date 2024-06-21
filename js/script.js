4fd8f2b24953fdcd8cc4191e40bf5d186f7f8131
JD Tadlock <jt7903@gmail.com>
2024-06-21 13:41:10 -0400

    finished object basics

--- js/script.js
+++ js/script.js
@@ -1,55 +1,43 @@
+const str = 'some string';
+const num = 10;
+const bool = true;
+const arr = ['John', 'Bob', 'Steve'];
 
-/* 
-Create a function that takes 3 arguments 
-- The first and second argument will be a number
-- The third argument will be an mathmatical operator (+, -, *, /)
-
-Example: If the operator is a '+' then you will add the two numbers together and console log the sum
-*/
-function calculate(num1, num2, operator) {
-  switch (operator) {
-    case '+':
-      return num1 + num2;
-    case '-':
-      return num1 - num2;
-    case '*':
-      return num1 * num2;
-    case '/':
-      return num1 / num2;
-    default:
-      return 'You must provide a valid operator';
+const data = {
+  name: 'JD',
+  age: 44,
+  info: {
+    location: 'Atl',
+    hobbies: [
+      {
+        name: 'Fishing',
+        frequency: 1
+      },
+      {
+        name: 'Pickleball',
+        frequency: 2
+      },
+      {
+        name: 'Another',
+        frequency: 3
+      }
+    ]
   }
-}
+};
 
-// Call your function 4 times with different numbers and operators to confirm that it is working correctly
-const sum = calculate(10, 20, '+');
-console.log(sum);
-
-// calculate(7, 89, '-');
-// calculate(25, 50, '*');
-// calculate(9, 3, '/');
-// calculate(9, 3, '&&&&&');
-// return is the function's answer
-// function add(num1, num2) {
-//   return 'another';
-// }
-
-// const answer = add();
-
-// console.log(answer);
-// add(5, 10);
-
-// console.log('something after our function call');
+console.log(data.info.hobbies[1].frequency)
 
 
-// if (operator === '+') {
-//   console.log(num1 + num2);
-// } else if (operator === '-') {
-//   console.log(num1 - num2);
-// } else if (operator === '*') {
-//   console.log(num1 * num2);
-// } else if (operator === '/') {
-//   console.log(num1 / num2);
-// } else {
-//   console.log('You must provide a valid operator')
-// }
+// const filtered = data.info.hobbies.filter(function (obj) {
+//   if (obj.frequency > 1) {
+//     return true;
+//   }
+// })
+
+// console.log(filtered);
+
+
+// data.name = 'Bob';
+// data.age++;
+
+// console.log(data.name);
