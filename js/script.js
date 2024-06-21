1d0ea5af3f769f6f1e25bfeda9aa73a4846af3f6
JD Tadlock <jt7903@gmail.com>
2024-06-21 09:18:45 -0400

    finished o.h. prompt/looping overview

--- js/script.js
+++ js/script.js
@@ -1,41 +1,16 @@
+const names = [];
+let addEmployee = true;
 
+while (addEmployee) {
+  let employeeName;
 
-// function myFirstFunc() {
-//   console.log('function has been called');
-// }
-
-// myFirstFunc();
-
-// function add(num1, num2) {
-//   console.log(num1 + num2);
-// }
-
-// add(5, 10);
-
-// add(30, 27);
-
-// function printValues(num, name, boolean, another) {
-//   console.log(another);
-// }
-
-// printValues(10, 'Allen', false, 'another');
-
-// const someStr = 'some string';
-
-// console.log(someStr[6]);
-
-function printNames(names) {
-  for (const name of names) {
-    console.log(name);
+  while (!employeeName) {
+    employeeName = prompt('Please provide the employee name name');
   }
+
+  names.push(employeeName);
+
+  addEmployee = confirm('Would you like to add another employee?');
 }
 
-// printNames(['Bob', 'Jimmy', 'Julie']);
-
-// printNames(['Jake', 'Frank', 'Katie']);
-
-const someName = 'JD';
-const h1 = document.querySelector('h1');
-
-h1.innerText = someName;
-
+console.log(names);
