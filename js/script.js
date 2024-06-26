e9b64d9da1c3a842a998aaba81c1f1eba738bf1d
JD Tadlock <jt7903@gmail.com>
2024-06-26 13:18:43 -0400

    finished modal example

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 let count = 5;
 const h1 = document.querySelector('h1');
 const modal = document.querySelector('.modal');
+const closeBtn = document.querySelector('#close-modal');
 
 function endGame() {
   modal.classList.remove('hide');
@@ -19,28 +20,12 @@ const interval = setInterval(function () {
 
 function closeModal() {
   modal.classList.add('hide');
-  console.log('body clicked');
 }
 
 document.body.addEventListener('click', closeModal);
 
-
-const outerBox = document.querySelector('.outer');
-
-outerBox.addEventListener('click', function (eventObj) {
+modal.addEventListener('click', function (eventObj) {
   eventObj.stopPropagation();
-
-  console.log('outer clicked!');
 });
 
-// setTimeout(function () {
-//   console.log('time is up!');
-// }, 8 * 1000);
-
-// function test() {
-//   return 'something'
-// }
-
-// const result = test();
-
-// console.log(result);
+closeBtn.addEventListener('click', closeModal);
