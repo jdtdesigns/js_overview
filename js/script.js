bdbb3a45d1d5f45a7b8a5a6fb4fcf787322d5093
JD Tadlock <jt7903@gmail.com>
2024-06-28 10:10:23 -0400

    keyboard events finished

--- js/script.js
+++ js/script.js
@@ -1,41 +1,38 @@
-const ul = document.querySelector('ul');
-const output = document.querySelector('#output');
-const addBtn = document.querySelector('#add');
+// // Grab/Target the h1 element by it's id
+const output = document.getElementById('key-output');
 
-function doSomething(eventObj) {
-  const el = eventObj.target;
 
-  if (el.tagName === 'LI') {
-    output.innerText = el.innerText;
-    // lis.forEach(function (li) {
-    //   li.style.background = '#555';
-    // });
 
-    eventObj.target.style.background = 'red';
+function keyPressed(eventObj) {
+  // Print the character(word) value of the key that was pressed
+  console.log(eventObj.key);
+
+  // Set the text of the h1 to something like 'Enter - 13'
+  output.innerText = eventObj.key + ' - ' + eventObj.keyCode;
+
+  // Checks if the enter key was pressed
+  if (eventObj.keyCode === 13) {
+    console.log(eventObj.key);
+    console.log('You have pressed the enter key');
   }
 }
 
-function addListItem() {
-  const li = document.createElement('li'); // <li></li>
-
-  li.innerText = 'New Item';
-
-  ul.append(li);
-}
-
-ul.addEventListener('click', doSomething);
-addBtn.addEventListener('click', addListItem);
-
-// const fruits = ['orange', 'apple', 'grape'];
-
-// fruits.forEach(function (li, index) {
-//   console.log(li, index);
-// })
+document.addEventListener('keydown', keyPressed);
 
 // const data = {
 //   name: 'JD',
 //   age: 44,
-//   info: {
-//     address: '555 coding st'
-//   }
 // }
+
+// // data.age = data.age + 1;
+// // console.log(data.age);
+
+// // function kitchen(blah, order) {
+// //   console.log(blah);
+// // }
+
+// // kitchen('medium steak', 10);
+
+// const str = 'some string';
+
+// console.log(str.length);
