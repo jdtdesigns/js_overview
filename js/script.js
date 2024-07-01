f9d6000611de6c8ce8debe72d7d1c7a2cbd43422
JD Tadlock <jt7903@gmail.com>
2024-07-01 10:30:35 -0400

    finished jquery overview

--- js/script.js
+++ js/script.js
@@ -1,38 +1,20 @@
-// // Grab/Target the h1 element by it's id
-const output = document.getElementById('key-output');
+const $saveBtn = $('#save-note');
 
+function createNote() {
+  const $titleInput = $('#title-input');
+  const $textInput = $('#text-input');
+  const $dateInput = $('#date-input');
+  const dateStr = $dateInput.val();
+  const currentTime = dayjs();
 
+  const dateTime = dayjs(dateStr)
+    .hour(currentTime.hour())
+    .minute(currentTime.minute())
+    .second(currentTime.second());
 
-function keyPressed(eventObj) {
-  // Print the character(word) value of the key that was pressed
-  console.log(eventObj.key);
-
-  // Set the text of the h1 to something like 'Enter - 13'
-  output.innerText = eventObj.key + ' - ' + eventObj.keyCode;
-
-  // Checks if the enter key was pressed
-  if (eventObj.keyCode === 13) {
-    console.log(eventObj.key);
-    console.log('You have pressed the enter key');
-  }
+  console.log($titleInput.val())
+  console.log(dateTime.format('ddd MMM, DD YYYY hh:mm:ss a'))
 }
 
-document.addEventListener('keydown', keyPressed);
-
-// const data = {
-//   name: 'JD',
-//   age: 44,
-// }
-
-// // data.age = data.age + 1;
-// // console.log(data.age);
-
-// // function kitchen(blah, order) {
-// //   console.log(blah);
-// // }
-
-// // kitchen('medium steak', 10);
-
-// const str = 'some string';
-
-// console.log(str.length);
+$saveBtn.on('click', createNote);
+$('#date-input').datepicker();
