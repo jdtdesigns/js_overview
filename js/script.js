1b57a6b75acfd26d17c983868f7f546b1e93956c
JD Tadlock <jt7903@gmail.com>
2024-07-01 12:47:31 -0400

    finished server request with user input

--- js/script.js
+++ js/script.js
@@ -1,41 +1,26 @@
+const weatherBtn = document.querySelector('#weather-btn');
 
-const apiKey = '3acc16ffae9e45df92a064e41646355f';
+function outputWeather() {
+  const cityInput = document.querySelector('#city-input');
+  const apiKey = '3acc16ffae9e45df92a064e41646355f';
 
-const url = `https://api.openweathermap.org/data/2.5/weather?q=atlanta&appid=${apiKey}&units=imperial`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=${apiKey}&units=imperial`;
 
-
-// We make a fetch(server-side) request to openweathermap for current weather data
-// The url must match the server's criteria for a request
-fetch(url)
-  // The first then callback function will be put to the side until the server responds back
-  .then(function (responseObj) {
-    // Passing the parsed json promise object to the next then
-    return responseObj.json();
-  })
-  .then(function (data) {
-    console.log(data);
-    const html = `
+  fetch(url)
+    .then(function (responseObj) {
+      return responseObj.json();
+    })
+    .then(function (data) {
+      const html = `
     <h2>Temp: ${data.main.temp}</h2>
     `;
-    const outputDiv = document.querySelector('.output');
+      const outputDiv = document.querySelector('.output');
 
-    outputDiv.innerHTML = html;
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+      outputDiv.innerHTML = html;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
 
-
-
-
-// fetch(url)
-//   .then(function (responseObj) {
-//     const dataPromise = responseObj.json();
-
-//     dataPromise.then(function (data) {
-//       console.log(data);
-//     });
-//   })
-//   .catch(function (error) {
-//     console.log(error);
-//   });
+weatherBtn.addEventListener('click', outputWeather);
