03ca126dece4317d1b3d11d4e2890e077f5fc033
JD Tadlock <jt7903@gmail.com>
2024-07-05 11:05:52 -0400

    finished 06 overview

--- js/script.js
+++ js/script.js
@@ -1,31 +1,54 @@
-const $charSelect = $('#char-select');
-const $searchBtn = $('#search-btn');
+const city = 'atlanta';
+const apiKey = '3acc16ffae9e45df92a064e41646355f';
+const $currentBtn = $('#current-btn');
 
-function getCharacters() {
-  const baseUrl = 'https://swapi.dev/api';
-  const option = $charSelect.val();
-  const url = `${baseUrl}/${option}`; // https://swapi.dev/api/people
+function getCurrentWeather(cityName) {
+  const currentURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=imperial`;
 
-  $.get(url)
-    // Once the json has been parsed, the callback function below will be called and it will be passed the data from the server
-    .then(function (data) {
-      const $outputDiv = $('.output');
-
-      $outputDiv.empty();
-
-      data.results.forEach(function (char) {
-        $outputDiv.append(`
-          <div>
-            <h3>${char.name}</h3>
-            <p>Birth Year: ${char.birth_year}</p>
-          </div>
-        `);
-      })
-    })
+  return $.get(currentURL)
 }
 
-function init() {
-  $searchBtn.on('click', getCharacters);
+
+
+function outputCurrentWeather(currentData) {
+  const $currentOutput = $('.current');
+
+  $currentOutput.html(`
+    <h2>Temp: ${currentData.main.temp} &deg;</h2>
+  `)
+
+  return currentData.coord;
 }
 
-init();
+
+function getForecastReport(locationData) {
+  const forecaseURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${locationData.lat}&lon=${locationData.lon}&appid=${apiKey}&units=imperial`;
+
+  return $.get(forecaseURL);
+}
+
+
+function outputForecastReport(forecastData) {
+  const $forecastOutput = $('.forecast');
+  // Filter out from the 40 weather objects you receive, the 5 noon time weather objects for the next 5 days
+  const filtered = forecastData.list.filter(function (weatherObj) {
+    if (weatherObj.dt_txt.includes('12')) return true;
+  });
+
+  filtered.forEach(function (weatherObj) {
+    $forecastOutput.append(`
+      <div>
+        <h2>${weatherObj.dt_txt}</h2>
+        <h3>Temp: ${weatherObj.main.temp}</h3>
+        <img src="https://openweathermap.org/img/wn/${weatherObj.weather[0].icon}@2x.png" alt="weather icon image">
+      </div>  
+    `)
+  })
+}
+
+$currentBtn.on('click', function () {
+  getCurrentWeather(city)
+    .then(outputCurrentWeather)
+    .then(getForecastReport)
+    .then(outputForecastReport);
+})
